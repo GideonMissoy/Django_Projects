ad5a9e9f869ed679049044bdcc58eb2cb813504e
GideonMissoy <giddykips2030@gmail.com>
2023-07-18 17:30:58 +0300

    Hello world.

--- HelloWorld/HelloWrld/settings.py
+++ HelloWorld/HelloWrld/settings.py
@@ -31,16 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    # My apps
-    'pages.apps.PagesConfig'
-
-    # Django default apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pages.apps.PagesConfig',
 ]
 
 MIDDLEWARE = [
