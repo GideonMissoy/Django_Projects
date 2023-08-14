d588a0e89b876e7a97a79a1f357a5347bdcc3a2c
GideonMissoy <giddykips2030@gmail.com>
2023-08-14 07:12:24 +0300

    Running tests.

--- MessageBoard/posts/tests.py
+++ MessageBoard/posts/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-
+from django.urls import reverse
 from .models import Post
 
 
@@ -10,3 +10,30 @@ class PostTests(TestCase):
     
     def test_model_content(self):
         self.assertEqual(self.post.text, "This is a test!")
+    
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+    
+    #def test_url_available_by_name(self):
+        #response =self.client.get(reverse("home"))
+        #s#elf.assertEqual(response.status_code, 200)
+    
+    #def test_template_name_correct(self):
+        #response = self.client.get(reverse("home"))
+        #self.assertTemplateUsed(response, "home.html")
+    
+    #def test_template_content(self):
+        #response = self.client.get(reverse("home"))
+        #self.assertContains(response, "This is a test!")
+    
+    #The 3 tests above are testing that the homepage works as expected:
+    #it uses the correct URL name, the intended template name,
+    # and contains expected content. They can therefore be combined
+    # into a single unit test
+
+    def test_homepage(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "home.html")
+        self.assertContains(response, "This is a test!")
